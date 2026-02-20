49eec4f0ee93229a877dfaad4ffc478ce12cedeb
Abbas Haider <AH1670@student.aru.ac.uk>
2026-02-20 04:47:06 +0000

    Auto-apply EF migrations on API startup

--- src/Daryva.Api/Program.cs
+++ src/Daryva.Api/Program.cs
@@ -119,6 +119,12 @@ else
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
